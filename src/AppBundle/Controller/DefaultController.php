1c8dbd550c5cd0ab615db4696c06c4c9af47127a
imafa82 <imafa82@gmail.com>
2016-10-19 20:18:18 +0200

    creazione form

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,33 +1,44 @@
 <?php
 
 namespace AppBundle\Controller;
-
+use AppBundle\Entity\User;
+use AppBundle\Form\UserType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+
 class DefaultController extends Controller
 {
     /**
-     * @Route("/{nome}/{cognome}", name="homepage")
+     * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
     {
-        echo $request->getBaseUrl();
-        echo "<br>";
-        echo $request->getRequestUri();
-        echo "<br>";
-        echo $request->getMethod();
-        echo "<br>";
-        echo $request->get('nome');
-        echo "<br>";
-        echo $request->get('cognome');
-        echo "<br>";
-        echo $request->query->get('citta');
-        $this->get('logger')->debug("debug di prova");
+        
+        
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'res' => $request,
-        ]);
+        return $this->render('default/index.html.twig', array());
     }
+    
+    
+     /**
+     * @Route("/users/new", name="users_new")
+     */
+    public function createUserAction(Request $request)
+    {
+        $user = new User();
+        $form= $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if($form->isValid() && $form->isSubmitted()){
+            return $this->redirectToRoute('homepage');
+        }
+
+        // replace this example code with whatever you need
+        return $this->render('default/users.html.twig', array('form' => $form->createView()));
+    }
+    
+    
+    
+    
 }
